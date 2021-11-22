9410a9e3f811efa3638cd8f966397db791225eb0
ReneFabricius <fabricius@stud.uniza.sk>
2021-11-22 10:15:26 +0100

    Added setuptools setup.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='WeightedEnsembles',
+      version='1.0',
+      description='Library for combining probabilistic classifiers',
+      author='Rene Fabricius',
+      author_email='quadrupedans@gmail.com',
+      packages=['weighted_ensembles'],
+      package_dir={'weighted_ensembles': ''}
+     )
