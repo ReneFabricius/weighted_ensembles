fd4ab059c7d3eb37373d68295528fe554e1df843
ReneFabricius <fabricius@stud.uniza.sk>
2022-10-25 10:38:31 +0200

    Updated required packages.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='weighted_ensembles',
-      version='1.0',
+setup(name='weighted-ensembles',
+      version='0.1.0',
       description='Library for combining probabilistic classifiers',
       author='Rene Fabricius',
       author_email='quadrupedans@gmail.com',
-      packages=['weensembles']
+      packages=find_packages(include=['weensembles', 'weensembles.*']),
+      install_requires=[
+            "torch>=1.12.0",
+            "torchvision>=0.13.0",
+            "pandas>=1.1.2",
+            "pytorch-minimize>=0.0.2",
+            "psutil",
+            "scikit-learn>=0.23.2",
+            "numpy>=1.21.6"
+      ],
+      python_requires=">=3.7"
      )
